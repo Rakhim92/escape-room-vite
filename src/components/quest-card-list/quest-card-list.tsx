26f170485cf34830ee04a57d594a57799901d9b2
Rakhim <pharmamystery19@gmail.com>
2026-05-19 15:20:07 +0700

    Начало. Нарезал все 6 страниц проекта WIP

---
+++ src/components/quest-card-list/quest-card-list.tsx
@@ -0,0 +1,9 @@
+import QuestCard from '../quest-card/quest-card';
+
+const QuestCardList = ():JSX.Element => (
+  <div className="cards-grid">
+    <QuestCard/>
+  </div>
+);
+
+export default QuestCardList;
