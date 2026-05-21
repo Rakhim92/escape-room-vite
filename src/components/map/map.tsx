e6c5d1956dcada002298fafe07348cea9d279638
Rakhim <pharmamystery19@gmail.com>
2026-05-21 20:42:54 +0700

    ContactsPage. Map. WIP

---
+++ src/components/map/map.tsx
@@ -0,0 +1,56 @@
+import {useRef, useEffect} from 'react';
+import {Icon, Marker, layerGroup} from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import useMap from './use-map';
+
+const defaultCustomIcon = new Icon({
+  iconUrl: 'img/pin.svg',
+  iconSize: [27, 39],
+  iconAnchor: [13.5, 39]
+});
+
+const currentCustomIcon = new Icon({
+  iconUrl: 'img/pin-active.svg',
+  iconSize: [27, 39],
+  iconAnchor: [13.5, 39]
+});
+
+function Map(props: MapProps): JSX.Element {
+  const {city, offers, selectedPoint} = props;
+
+  const mapRef = useRef(null);
+  const map = useMap(mapRef, city);
+
+  useEffect(() => {
+    if (map) {
+      map.setView([city.location.latitude, city.location.longitude], city.location.zoom);
+    }
+  }, [city, map]);
+
+  useEffect(() => {
+    if (map && offers) {
+      const markerLayer = layerGroup().addTo(map);
+
+      offers.forEach((offer) => {
+        const marker = new Marker({
+          lat: offer.location.latitude,
+          lng: offer.location.longitude
+        });
+
+        const isCurrent = !!selectedPoint && offer.id === selectedPoint.id;
+
+        marker
+          .setIcon(isCurrent ? currentCustomIcon : defaultCustomIcon)
+          .addTo(markerLayer);
+      });
+
+      return () => {
+        map.removeLayer(markerLayer);
+      };
+    }
+  }, [map, offers, selectedPoint]);
+
+  return <section className={`${ClassNamesForMap} map`} ref={mapRef}/>;
+}
+
+export default Map;
